47e8e43f8b60c3663d72fe60817f8b4ad5b0b363
PYETRO FERREIRA DE SOUZA <25205137@cfp127aluno.local>
2025-11-03 16:58:14 -0300

    Finalizado a tabuada

--- src/br/senai/sp/jandira/tabuada/TabuadaApp.java
+++ src/br/senai/sp/jandira/tabuada/TabuadaApp.java
@@ -1,4 +1,14 @@
 package br.senai.sp.jandira.tabuada;
 
+import br.senai.sp.jandira.tabuada.model.Usuario;
+
 public class TabuadaApp {
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario();
+        usuario.obterDados();
+
+
+
+
+    }
 }
--- src/br/senai/sp/jandira/tabuada/model/Usuario.java
+++ src/br/senai/sp/jandira/tabuada/model/Usuario.java
@@ -1,4 +1,55 @@
 package br.senai.sp.jandira.tabuada.model;
 
+import java.util.Scanner;
+
 public class Usuario {
+
+      public int multiplicando;
+       public int multiplicadorInicial;
+        public int multiplicadorFinal;
+        public int tamanho;
+        public String[] tabuada;
+
+        public void obterDados(){
+            Scanner leitor = new Scanner(System.in);
+
+            System.out.print("Qual é o valor do multiplicando?: ");
+            multiplicando = leitor.nextInt();
+
+            System.out.print("Qual o valor do multiplicadorInicial?: ");
+            multiplicadorInicial = leitor.nextInt();
+
+            System.out.print("Qual o valor do multiplicadorFinal?: ");
+            multiplicadorFinal = leitor.nextInt();
+
+            calcularTabuada();
+        }
+        public void calcularTabuada(){
+            int apoio = 0;
+            if (multiplicadorFinal < multiplicadorInicial){ // se o usuário colocar um multiplicador final colocar um valor maior que o inicial (que é impossível de calcular)
+                apoio = multiplicadorFinal; //A variavel apoio vai pegar o número do multiplicador final
+                multiplicadorFinal = multiplicadorInicial; //O número do multiplicador Final vai virar o multiplicador Inicial
+                multiplicadorInicial = apoio; //O multiplicador final Ficando com o número do multiplicador inical, ele recepera o numero guardado da variavel apoio, assim, invertendo
+            }
+            int tamanho = multiplicadorFinal - multiplicadorInicial + 1;
+            tabuada = new String [tamanho];
+
+            int i = 0;
+            while (i < tamanho){
+                int produto =  multiplicando * multiplicadorInicial;
+                tabuada[i] = multiplicando + " X "  + multiplicadorInicial + " = " + produto;
+                multiplicadorInicial = multiplicadorInicial + 1;
+                i =  i + 1;
+            }
+        exibirDados();
+    }
+    public void exibirDados(){
+        System.out.print("Resultado da sua tabuada: ");
+
+        int i = 0;
+        while (i < tabuada.length){
+            System.out.println(tabuada[i]);
+            i++; //i = i = 1
+        }
+    }
 }
